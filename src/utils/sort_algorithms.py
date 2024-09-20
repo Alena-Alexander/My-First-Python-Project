5134a7592cfb2022f486bc41ac313060cc383ec9
Alena-alexander <alena.alexander04@gmail.com>
2024-09-20 17:19:56 -0400

    - Added empty file - Added empty file - Improved notes - Worked on challenge _ Worked on challenge

--- src/utils/sort_algorithms.py
+++ src/utils/sort_algorithms.py
@@ -9,11 +9,40 @@ def bubble_sort(values: list) -> list:
     """
     Description:
     ===========
-    This function iterates over an array in ascending order,
-    compares the elements in the array and swaps them.
+
+    Bubble sort is a sorting algorithm that compares two adjacent elements
+    and swaps them until they are in the intended order.
+
+    Working of Bubble Sort:
+    ======================
+
+    Suppose we are trying to sort the elements in ascending order.
+
+    1. First Iteration (Compare and Swap)
+
+    1.2. Starting from the first index :math:`n`, compare the first and the second element
+    at index :math:`n + 1`.
+
+    2. If the first element is greater than the second element, they are swapped.
+
+    3. Now, compare the second and the third elements. Swap them if they are not in order.
+
+    4. The above process goes on until the last element.
+
+    Pseudocode:
+    ==========
+
+    >>> bubbleSort(array)
+    >>> for i <- 1 to sizeOfArray - 1
+    >>>     for j <- 1 to sizeOfArray - 1 - i
+    >>>         if leftElement > rightElement
+    >>>             swap leftElement and rightElement
+    >>> end bubbleSort
+
 
     Notes:
     =====
+
     Sorting an array means to arrange the elements in the array in a certain order,
     either ascending or descending. In general terms, Ascending means smallest to
     largest, 0 to 9, and/or A to Z and Descending means largest to smallest, 9 to 0,
@@ -21,20 +50,21 @@ def bubble_sort(values: list) -> list:
 
     Example:
     =======
+
     >>> print(bubble_sort([5,8,2,5,4,8]))
     >>> [2,4,5,5,8,8]
 
-    :param values: list: List of values
-    :return: list
+    :param values: list: A collection of values.
+    :return: list: a collection of sorted values
     """
     start_time = time.time()
     # print(values)
     number_of_elements = len(values)
     # Iterate in descending order, from the number_of_elements (last index) to 0, at of step -1
-    for n in np.arange(number_of_elements - 1, 0, -1):
+    for n in np.arange(start=number_of_elements - 1, stop=0, step=-1):    # <== direction
         # print(f"Descending pass index: {n}")
         # Iterate in ascending order, from 0 to n
-        for i in range(n):
+        for i in np.arange(start=0, stop=n, step=1):  # ==> direction
             # print(f"Ascending pass index: {i}")
             # Compare the previous and next value
             if values[i] > values[i + 1]:
@@ -46,37 +76,80 @@ def bubble_sort(values: list) -> list:
                 # assign the swapped values
                 values[i] = next_value
                 values[i + 1] = previous_value
-    # print(f"Duration: {time.time() - start_time}")
+    print(f"Duration: {time.time() - start_time}")
     return values
 
 
 def bubble_sort_opt(values: list) -> list:
     """
-    Description:
+     Description:
     ===========
 
+    Bubble sort optimized is a sorting algorithm that compares two adjacent elements
+    and swaps them until they are in the intended order.
+
+    Working of Bubble Sort:
+    ======================
+
+    Suppose we are trying to sort the elements in ascending order. In this
+    optimized approach we can improve it by using one extra flag ``swapped``. No more
+    swaps indicate the completion of sorting. If the list is already sorted,
+    we can use this flag to skip the remaining passes
+
+    1. First Iteration (Compare and Swap).
+
+    1.2. Starting from the first index :math:`n`, compare the first and the second element
+    at index :math:`n + 1`.
+
+    2. If the first element is greater than the second element, they are swapped.
+
+    3. Now, compare the second and the third elements. Swap them if they are not in order.
+
+    4. The above process goes on until the last element.
 
     Notes:
     =====
+
     Sorting an array means to arrange the elements in the array in a certain order,
     either ascending or descending. In general terms, Ascending means smallest to
     largest, 0 to 9, and/or A to Z and Descending means largest to smallest, 9 to 0,
     and/or Z to A.
 
-    :param values: list:
-    :return: list
+    Pseudocode:
+    ==========
+
+    >>> bubble_sort_opt(array)
+    >>> flag = 1
+    >>> for i <- 1 to sizeOfArray - 1
+    >>>     if flag == 1:
+    >>>         flag = 0
+    >>>         for j <- 1 to sizeOfArray - 1 - i
+    >>>             if leftElement > rightElement
+    >>>                 swap leftElement and rightElement
+    >>>                 flag = 1
+    >>> end bubble_sort_opt
+
+
+    Example:
+    =======
+
+    >>> print(bubble_sort_opt([5,8,2,5,4,8]))
+    >>> [2,4,5,5,8,8]
+
+    :param values: list: A collection of values.
+    :return: list: : a collection of sorted values
     """
     start_time = time.time()
     # print(values)
     number_of_elements = len(values)
-    precious_value, index, next_value, swapped = 1, 1, 1, 1
+    swapped = 1
     # Iterate in descending order, from the number_of_elements (last index) to 0, at of step -1
-    for n in np.arange(number_of_elements - 1, 0, -1):
+    for index in np.arange(start=number_of_elements - 1, stop=0, step=-1):
         # print(f"Descending pass index: {n}, Swapped: {swapped}")
         if swapped:
             swapped = 0
             # Iterate in ascending order, from 0 to n
-            for i in range(n):
+            for i in np.arange(start=0, stop=index, step=1):
                 # print(f"Ascending pass index: {i}, if {values[i]} > {values[i + 1]} = {values[i] > values[i + 1]}")
                 # Compare the previous and next value
                 if values[i] > values[i + 1]:
@@ -88,25 +161,50 @@ def bubble_sort_opt(values: list) -> list:
                     values[i] = next_value
                     values[i + 1] = previous_value
                     swapped = 1
-    # print(f"Duration: {time.time() - start_time}")
+    print(f"Duration: {time.time() - start_time}")
     return values
 
 
 def bubble_sort_by_order_of_2(values: list) -> list:
     """
-    Description:
+     Description:
     ===========
 
+    bubble_sort_by_order_of_2 is a sorting algorithm that compares two adjacent elements
+    and swaps them until they are in the intended order.
+
+    Working of Bubble Sort:
+    ======================
+
+    Suppose we are trying to sort the elements in ascending order.
+
+    1. First Iteration (Compare and Swap).
+
+    1.2. Starting from the first index :math:`n`, compare the first and the second element
+    at index :math:`n + 2`.
+
+    2. If the first element is greater than the second element, they are swapped.
+
+    3. Now, compare the second and the third elements. Swap them if they are not in order.
+
+    4. The above process goes on until the last element.
 
     Notes:
     =====
+
     Sorting an array means to arrange the elements in the array in a certain order,
     either ascending or descending. In general terms, Ascending means smallest to
     largest, 0 to 9, and/or A to Z and Descending means largest to smallest, 9 to 0,
     and/or Z to A.
 
-    :param values: list:
-    :return: list
+    Example:
+    =======
+
+    >>> print(bubble_sort_by_order_of_2([5,8,2,5,4,8]))
+    >>> [2,4,5,5,8,8]
+
+    :param values: list: A collection of values.
+    :return: list: a collection of sorted values
     """
     start_time = time.time()
     # print(values)
@@ -115,7 +213,7 @@ def bubble_sort_by_order_of_2(values: list) -> list:
     for n in np.arange(number_of_elements - 1, 0, -2):
         # print(f"Descending pass index: {n}")
         # Iterate in ascending order, from 0 to n
-        for i in range(n):
+        for i in np.arange(n):
             # print(f"Ascending pass index: {i}")
             if i % 2 != 0:
                 # Compare the previous and next value
@@ -128,7 +226,7 @@ def bubble_sort_by_order_of_2(values: list) -> list:
                     # assign the swapped values
                     values[i - 1: i + 1] = next_value
                     values[i + 1: i + 3] = previous_value
-    # print(f"Duration: {time.time() - start_time}")
+    print(f"Duration: {time.time() - start_time}")
     return values
 
 
@@ -137,9 +235,30 @@ def create_nested_list(values: list) -> list:
     number_of_elements = len(values)
     nested_list = []
     # Iterate in descending order, from the number_of_elements (last index) to 0, at of step -1
-    for n in range(0, number_of_elements, 2):
+    for n in np.arange(start=0, stop=number_of_elements, step=2):
         # print(f"iteration: {n}")
         nested_list.append(values[n: n + 2])
     # print(f"Duration: {time.time() - start_time}")
     return nested_list
 
+
+def num_of_identical_scores(sorted_list: list) -> int:
+    sorted_list = bubble_sort_by_order_of_2(sorted_list)
+    # It holds the value of the number of persons that have the same score,
+    # we initialize it to 1, since at least one score will be the lowest.
+    same_score_count = 1
+    arr_length = len(sorted_list) - 2
+    for i in range(arr_length):
+        if i % 2 == 1:  # We use the number `2` because it represents how many elements identify a single person data.
+            print(f"Index: {i} ==> comparing: {sorted_list[i]} & {sorted_list[i + 2]}")
+            if sorted_list[i] == sorted_list[i + 2]:
+                same_score_count += 1
+    return same_score_count
+
+
+if __name__ == "__main__":
+    unsorted_values = [int(100 * a) for a in np.random.random(1000)]
+    sorted_values1 = bubble_sort(unsorted_values.copy())
+    # print(sorted_values1)
+    sorted_values2 = bubble_sort_opt(unsorted_values.copy())
+    # print(sorted_values2)
